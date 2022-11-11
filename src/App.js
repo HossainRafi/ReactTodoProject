be5af1fc159a95664c6922d90eff5d2a7aac9429
HossainRafi <hossainrafi71@gmail.com>
2022-11-11 21:47:34 +0600

    form done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import React from "react";
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
 };
 
 function App() {
   return (
     <div className={style.bg}>
       <div className={style.container}>
-        <div className={style.heading}>What To-Do</div>
+        <h3 className={style.heading}>What To-Do</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add Todo" />
+          <button className={style.button}>Add</button>
+        </form>
       </div>
     </div>
   );
