30a50be2a156e029d7f3cd52e2effd6f4145dd20
HossainRafi <hossainrafi71@gmail.com>
2022-11-11 22:08:25 +0600

    delete button added

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const style = {
   li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
@@ -9,14 +10,15 @@ const style = {
   button: `cursor-pointer flex items-center`,
 };
 
-const Todo = ({todo}) => {
+const Todo = ({ todo }) => {
   return (
     <div>
-          <li className={style.li}>
-              <div className={style.row}>
-                  <input type="checkbox" />
-                  <p className={style.text}>{todo}</p>
-              </div>
+      <li className={style.li}>
+        <div className={style.row}>
+          <input type="checkbox" />
+          <p className={style.text}>{todo}</p>
+        </div>
+        <button>{<FaRegTrashAlt />}</button>
       </li>
     </div>
   );
